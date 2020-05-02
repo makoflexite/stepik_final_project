1632663a9369b7f12378e12bc5d1ae24b6a9cf37
Mariya <mako@flexite.com>
2020-05-02 16:55:09 +0300

    changed using locators

--- pages/main_page.py
+++ pages/main_page.py
@@ -2,11 +2,12 @@
 # from selenium import webdriver
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
+from .locators import MainPageLocators
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK) # *says that we gave pair of parameters and this cortege should be unpacked
         login_link.click()
 
     def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
