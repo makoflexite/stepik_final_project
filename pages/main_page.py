891ee56f6d35028ead0e24b48565a25e43095cd5
Mariya <mako@flexite.com>
2020-05-06 14:23:43 +0300

    added locators for login_link and login_link_invalid to locators.py. methods go_to_login_page and should_be_login_link are moved  to base_page.py from main_page.py. methods test_guest_should_see_login_link_on_product_page and test_guest_can_go_to_login_page_from_product_page are added to test_product_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -6,9 +6,7 @@ from .login_page import LoginPage
 from .locators import MainPageLocators
 
 class MainPage(BasePage):
-    def go_to_login_page(self):
-        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK) # *says that we gave pair of parameters and this cortege should be unpacked
-        login_link.click()
 
-    def should_be_login_link(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+    def __init__(self, *args, **kwargs):
+        """ Конструктор с ключевым словом super вызывает конструктор класса предка и передает ему все те аргументы, которые мы передали в конструктор MainPage"""
+        super(MainPage, self).__init__(*args, **kwargs)
